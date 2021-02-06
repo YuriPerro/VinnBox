c4e6e8fa20d13e6f484b7d3e177df290197efb95
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-02-06 17:43:57 -0300

    feat(Components): Cria BaseButton

---
+++ src/components/BaseButton/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+export default function BaseButton(props) {
+  const { type = "button", style, children } = props;
+
+  return (
+    <button className="base-button" type={type} style={style}>
+      {children}
+    </button>
+  );
+}
