4b8c0dc4784a2caa54021275f58ec35daa74d37e
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-02-06 17:53:21 -0300

    feat(Register): Adiciona link para Login

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./styles.scss";
+import { Link } from "wouter";
 
 const About = () => {
   return (
     <div className="register-wrapper">
       <h2>About Page</h2>
+
+      <Link href={"/login"}>Login</Link>
     </div>
   );
 };
