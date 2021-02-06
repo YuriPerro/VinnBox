76c0bf7154a30f93ed4fa42b66aa411f5fc5712c
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-02-06 17:34:15 -0300

    feat(Register): Adiciona estilo padrao de toda pagina

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./styles.scss";
 
 const About = () => {
   return (
-    <div>
+    <div className="register-wrapper">
       <h2>About Page</h2>
     </div>
   );
