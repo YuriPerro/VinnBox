88742fbd77df83682169f67e88b346ac56a6f4f8
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-02-27 15:56:00 -0300

    add Modal na Home

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,30 +1,28 @@
 import "./styles.scss";
 
-import sideBarIcon from '../../assets/icons/sideBarIcon.svg'
-import homeIcon from '../../assets/icons/homeIcon.svg'
-import plusIcon from '../../assets/icons/plusIcon.svg'
-import logoutIcon from '../../assets/icons/logoutIcon.svg'
+import sideBarIcon from "../../assets/icons/sideBarIcon.svg";
+import homeIcon from "../../assets/icons/homeIcon.svg";
+import plusIcon from "../../assets/icons/plusIcon.svg";
+import logoutIcon from "../../assets/icons/logoutIcon.svg";
 
-export default function SideBar() {
-    return (
-        <div className="container">
-            <input
-                type="image"
-                src={sideBarIcon}
-            />
-            <input
-                type="image"
-                src={homeIcon}
-            />
-            <input
-                type="image"
-                src={plusIcon}
-            />
-            <div className="container-bottom">
-                <input type="image" src={logoutIcon} />
-                <div className="line-bottom" />
-                <h3>VB</h3>
-            </div>
-        </div>
-    )
+export default function SideBar(props) {
+  const { openModal } = props;
+
+  return (
+    <div className="sidebar-container">
+      <input type="image" src={sideBarIcon} alt="menu" />
+      <input type="image" src={homeIcon} alt="home" />
+      <input
+        type="image"
+        src={plusIcon}
+        alt="add template"
+        onClick={openModal}
+      />
+      <div className="container-bottom">
+        <input type="image" src={logoutIcon} alt="logout" />
+        <div className="line-bottom" />
+        <h3>VB</h3>
+      </div>
+    </div>
+  );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.scss";
 
-import SideBar from '../../components/SideBar/index';
+import { BaseModal, SideBar } from "../../components";
+import BodyModalTemplate from "./BodyModalTemplate";
 
 function Home() {
+  const [mdVisibility, setMdVisibility] = useState(false);
+  const [templateForm, setTemplateForm] = useState({
+    name: "",
+    description: "",
+    color: "#ff9c9c",
+  });
+
+  function handleTemplateFormChange(event) {
+    const { value, name } = event.target;
+    setTemplateForm((prev) => ({ ...prev, [name]: value }));
+  }
+
   return (
-    <div className="container">
-      <h2>
-      </h2>
+    <div className="home-wrapper">
+      <SideBar openModal={() => setMdVisibility(true)} />
 
-      <SideBar/>
+      <div className="content">
+        <h2>Home</h2>
+        <BaseModal visibility={mdVisibility} setVisibility={setMdVisibility}>
+          <BodyModalTemplate
+            form={templateForm}
+            onChangeForm={handleTemplateFormChange}
+          />
+        </BaseModal>
 
+        <br />
+        {templateForm.name}
+        <br />
+        {templateForm.description}
+        <br />
+        {templateForm.color}
+        <br />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home;
