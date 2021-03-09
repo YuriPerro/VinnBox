45fb362a5d774fa1a71b5220bb7b144019a9c16f
Yuri Perro <yuribaumgartner@ice.ufjf.br>
2021-03-09 16:10:57 -0300

    Adicionando login, cadastro e validação dos formularios

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import "./styles.scss";
 
 import { Link, useLocation } from "wouter";
-import { BaseButton } from "../../components";
+import { BaseButton, BaseModal } from "../../components";
 import dividerCard from "../../assets/images/card-divider.png";
 import footerImgLogin from "../../assets/images/login-footer-img.png";
 import VinnBoxLogo from "../../assets/images/VINNBOX.png";
+import Cookies from 'js-cookie';
 
 const Login = () => {
   const [form, setForm] = useState({
@@ -13,20 +14,119 @@ const Login = () => {
     password: "",
   });
   const [, setLocation] = useLocation();
+  const [openModal, setOpenModal] = useState(false);
+  const [showPass, setShowPass] = useState(false);
+  const [recPass, setRecPass] = useState("");
 
   function handleInputChange(event) {
     const { value, name } = event.target;
     setForm((prev) => ({ ...prev, [name]: value }));
   }
 
-  function submitForm(event) {
+  async function submitForm(event) {
     event.preventDefault();
-    console.log(form);
-    setLocation("/home");
+    var email = document.getElementById("email").value;
+    var pass = document.getElementById("pass").value;
+
+    var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+    if (!reg.test(email))
+      alert("Email inválido!")
+    else {
+      const checkUser = await usersExists(email, pass, false);
+      if (checkUser) {
+        setLocation("/home");
+      }
+    }
+  }
+
+  function usersExists(email, senha, recSenha) {
+    return new Promise(function (result, reject) {
+      let usersData = Cookies.getJSON('users');
+      if (usersData) {
+        const userDB = Object.keys(usersData)
+          .filter(index => usersData[index].email === email)
+          .map(index => {
+            return usersData[index];
+          });
+        if (recSenha) {
+          console.log(userDB)
+          if (userDB.length > 0) {
+            setRecPass(userDB[0].senha);
+            setShowPass(true);
+          }
+          else {
+            alert("Email não cadastrado! Crie uma conta antes de continuar.")
+            result(false);
+          }
+        }
+        else {
+          if (userDB.length > 0) {
+            if (userDB[0].senha !== senha) {
+              alert("Senha inválida, tente novamente!");
+              result(false);
+            }
+            else
+              result(true);
+          }
+          else {
+            alert("Email não cadastrado! Crie uma conta antes de continuar.")
+            result(false);
+          }
+        }
+      }
+      else
+        result(false);
+    })
+  }
+
+  async function recPassword() {
+    var recEmail = document.getElementById("recEmail").value;
+
+    var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+    if (!reg.test(recEmail))
+      alert("Email inválido!")
+    else
+      await usersExists(recEmail, "", true);
+  }
+
+  function closeModalRec() {
+    setRecPass("");
+    setShowPass(false);
+    setOpenModal(false)
   }
 
   return (
     <div className="login-wrapper">
+
+      <BaseModal
+        title={"Adicionar Template"}
+        visibility={openModal}
+        setVisibility={setOpenModal}
+      >
+        {!showPass ?
+          <>
+            <input
+              placeholder="Digite seu email de recuperação"
+              id="recEmail"
+            />
+            <BaseButton onClick={() => recPassword()} type={"button"}> Recuperar senha </BaseButton>
+          </>
+          :
+          <>
+            <a
+              style={{
+                textAlign: "center",
+                marginTop: 25,
+                marginBottom: 25,
+                fontFamily: "monospace",
+                fontWeight: 600,
+                fontSize: 20
+              }}> Sua senha é: {recPass}
+            </a>
+            <BaseButton onClick={() => closeModalRec()} type={"submit"}> Confirmar </BaseButton>
+          </>
+        }
+      </BaseModal>
       <img
         src={VinnBoxLogo}
         width={90}
@@ -34,10 +134,8 @@ const Login = () => {
         style={{ alignSelf: "center" }}
         alt="logo"
       />
-
       <div className="card">
         <h2 className="title">Faça login</h2>
-
         <div className="body">
           <form onSubmit={submitForm} className="form-group">
             <label htmlFor="email">E-mail</label>
@@ -60,9 +158,11 @@ const Login = () => {
               onChange={handleInputChange}
             />
 
-            <Link href="/login" className="forgot-pass-link">
+
+            <a className="forgot-pass-link" onClick={() => setOpenModal(true)}>
               Esqueceu a senha ?
-            </Link>
+            </a>
+
             <BaseButton type={"submit"}>Entrar</BaseButton>
           </form>
 
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -5,9 +5,12 @@ import { Link, useLocation } from "wouter";
 import { BaseButton } from "../../components";
 import dividerCard from "../../assets/images/card-divider.png";
 import footerImgLogin from "../../assets/images/login-footer-img.png";
+import VinnBoxLogo from "../../assets/images/VINNBOX.png";
+
+import Cookies from 'js-cookie';
 
 const Cadastro = () => {
-  const [form] = useState({
+  const [form, setForm] = useState({
     nome: "",
     email: "",
     password: "",
@@ -15,16 +18,81 @@ const Cadastro = () => {
   });
   const [, setLocation] = useLocation();
 
-  function submitForm(event) {
+  async function submitForm(event) {
     event.preventDefault();
-    console.log(form);
-    setLocation("/home");
+    var nome = document.getElementById("nome").value;
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("pass").value;
+    var conf = document.getElementById("conf").value;
+
+    if (String(nome).length <= 2)
+      alert("Nome curto demais!");
+    else {
+      var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+      if (!reg.test(email))
+        alert("Email inválido!")
+      else {
+        var regSenha = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,}$/;
+        if (!regSenha.test(senha))
+          alert("Senha fraca. Tente adicionar pelo menos 4 caracteres, 1 letra e 1 número!")
+        else {
+          if (senha !== conf) {
+            alert("As senhas devem ser iguais!");
+          }
+          else {
+            let formData = {
+              nome: nome,
+              email: email,
+              senha: senha
+            }
+
+            if (Cookies.get('users')) {
+              if (await usersExists(email)) {
+                alert("Já existe um usuário cadastrado com esse email!")
+              }
+              else {
+                let usersData = Cookies.getJSON('users');
+                usersData.push(formData);
+                Cookies.set("users", JSON.stringify(usersData));
+                setLocation("/home");
+              }
+            }
+            else {
+              let arrayData = [];
+              arrayData.push(formData);
+              Cookies.set("users", JSON.stringify(arrayData));
+              setLocation("/home");
+            }
+          }
+        }
+      }
+    }
+  }
+
+  function usersExists(email) {
+    return new Promise(function (result, reject) {
+      let usersData = Cookies.getJSON('users');
+      if (usersData) {
+        const userDB = Object.keys(usersData).filter(index => usersData[index].email === email);
+        if (userDB.length > 0)
+          result(true);
+        else
+          result(false);
+      }
+      else
+        result(false);
+    })
   }
 
   return (
     <div className="register-wrapper">
-      <h1 className="title">VinnBox</h1>
-
+      <img
+        src={VinnBoxLogo}
+        width={90}
+        height={60}
+        style={{ alignSelf: "center" }}
+        alt="logo"
+      />
       <div className="card">
         <h2 className="title">Cadastre-se</h2>
         <div className="body">
@@ -54,7 +122,7 @@ const Cadastro = () => {
 
             <label htmlFor="conf">Confirme sua Senha</label>
             <input
-              type="confirmation"
+              type="password"
               name="confirmation"
               id="conf"
               placeholder="Digite sua senha"
