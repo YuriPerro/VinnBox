e973bca2f24e783715b53e74cf4608cb4a6add92
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-03-09 15:23:46 -0300

    Corrige click de fechar do modal

--- frontend/src/components/BaseModal/index.js
+++ frontend/src/components/BaseModal/index.js
@@ -2,16 +2,19 @@ import React from "react";
 import "./styles.scss";
 
 const BaseModal = (props) => {
-  const {
-    title = "Empty",
-    visibility = false,
-    setVisibility,
-    children,
-  } = props;
+  const { title = "Empty", visibility = false, setVisibility, children } = props;
 
   return (
     visibility && (
-      <div className="modal-overlay" onClick={() => setVisibility(false)}>
+      <div className="modal-container">
+        <div
+          className="modal-overlay"
+          onClick={(e) => {
+            setVisibility(false);
+            e.stopPropagation();
+          }}
+        ></div>
+
         <div className="modal" onClick={(e) => e.stopPropagation()}>
           <header>
             <h2>{title}</h2>
